6c192afc7c0ac09c1b82191a72c151b3aaa14576
asad521 <63185364+asad521@users.noreply.github.com>
2022-01-27 14:48:04 +0500

    static web deployment on netlify react app

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>netlify static app deployment on netlify</h2>
+      <h4>Here we first make netlify.toml file in public. first we define command build.com
+        Then we will give path of build.
+      </h4>
+      <h4>then go you github and make a new repo.</h4>
+    </div>
+  );
+}
+
+export default App;
