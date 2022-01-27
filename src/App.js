34404d649eedddbc8062e46d70c7a0205d847339
asad521 <63185364+asad521@users.noreply.github.com>
2022-01-27 14:55:49 +0500

    static web deployment on netlify react app

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <h4>Here we first make netlify.toml file in public. first we define command build.com
         Then we will give path of build.
       </h4>
-      <h4>then go you github and make a new repo.</h4>
+      <h4>then go you github and make a new repo and commit all code.</h4>
     </div>
   );
 }
